0dbca0b08ff36887b8e286e23a83eafc116b0bd7
Soham Das <138379203+sohamdas1105@users.noreply.github.com>
2024-01-21 09:45:41 +0530

    Create string and functions.kt

---
+++ string and functions.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbasics
+fun main(){
+
+var myname= "Soham"
+   // myname= "Das"
+    println(myname)
+println(myname.uppercase()) //easily use functions
+    println(myname.lowercase())
+    println(myname+ " Das")
+}
+// output will be 
+//Soham
+//SOHAM
+//soham
+//Soham Das
