7dd02354133a6cef798d9a33889aab165247a66c
Soham Das <138379203+sohamdas1105@users.noreply.github.com>
2024-01-21 12:40:38 +0530

    Create inputhandling.kt

---
+++ inputhandling.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main(){
+    var inputstring = 0
+    println("Please eneter a number")
+    inputstring = readln().toInt()
+    val number = 5
+    val mul = inputstring*number
+    println(" the multiplication of $inputstring and $number is $mul ")
+}
